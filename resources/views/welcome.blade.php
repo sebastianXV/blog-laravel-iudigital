35219af214591af24ac06ec78d919fbf1aa43824
sebastianXV <69022009+sebastianXV@users.noreply.github.com>
2023-05-22 14:55:36 -0500

    updating main views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.app')
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    
-        <!-- Scripts -->
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    
-        <title>Welcome</title>
-</head>
-<body>
-    <h1>Pagina de inicio </h1>
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+@section('content')
+    @auth
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Bienvenido(a) {{ Auth::user()->name }}</h1>
+                    <p>Explora, descubre y comparte tus experiencias en el mundo de los videojuegos.</p>
+                    <a href="{{ route('home') }}" class="btn btn-primary btn-lg">Ir a la página principal</a>
+                </div>
+            </div>
         </div>
-      </div>
-</body>
-</html>
+    @else
+        <div class="jumbotron text-center">
+            <h1 class="display-4">Bienvenido al Mundo de los Videojuegos</h1>
+            <p class="lead">Explora, descubre y comparte tus experiencias en el mundo de los videojuegos.</p>
+            <hr class="my-4">
+            <p>Únete a nuestra comunidad y comparte tus reseñas, avances, trucos y más.</p>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="img/inicio1.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Call of Duty Black Ops 3</h5>
+                            <p class="card-text">Shooter en primera persona.</p>
+                            <a href="#" class="btn btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="img/inicio2.webp" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Grand Theft Auto V</h5>
+                            <p class="card-text">Mundo abierto.</p>
+                            <a href="#" class="btn btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img class="card-img-top" src="img/inicio3.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">E Football</h5>
+                            <p class="card-text">Vive el football mas realista.</p>
+                            <a href="#" class="btn btn-primary">Ver más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endauth
+@endsection
